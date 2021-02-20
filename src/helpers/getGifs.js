bde97c2c556f493b93bd361d5ad8c672016e35f8
Andres Riber@ <andresribera1@hotmail.com>
2021-02-20 11:32:18 -0400

    docs actualizado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export  const getGifs = async( category ) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=4tK1ZPimokFRyeEQ1mnvRoux8939yWqv`
+  const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=4tK1ZPimokFRyeEQ1mnvRoux8939yWqv`
   const resp = await fetch( url );
   const {data} = await resp.json();
 
